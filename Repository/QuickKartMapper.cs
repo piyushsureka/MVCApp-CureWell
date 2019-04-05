3be280748c1511d800cb450ce46ec1e9535bbeab
piyushsureka <30704103+piyushsureka@users.noreply.github.com>
2019-04-05 11:19:08 +0530

    Add files via upload

---
+++ Repository/QuickKartMapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using QuickKartDataAccessLayer.Models;
+using QuickKartCoreMVCApp.Models;
+
+
+namespace QuickKartCoreMVCApp.Repository
+{
+    public class QuickKartMapper : Profile
+    {
+        public QuickKartMapper()
+        {
+            CreateMap<QuickKartDataAccessLayer.Models.Products, Models.Products>();
+            CreateMap<Models.Products, QuickKartDataAccessLayer.Models.Products>();
+            CreateMap<QuickKartDataAccessLayer.Models.Categories, Models.Categories>();
+            CreateMap<Models.Categories, QuickKartDataAccessLayer.Models.Categories>();
+            CreateMap<Models.PurchaseDetails, QuickKartDataAccessLayer.Models.PurchaseDetails>();
+
+        }
+    }
+
+}
